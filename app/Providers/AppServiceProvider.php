728a544c3235c1ad3861529888baef45cad96446
Jesus Valera <jesus1902@outlook.com>
2019-06-08 23:55:40 +0200

    Change singleton service by bind method.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(TeamRepository::class, EloquentTeamRepository::class);
+        $this->app->bind(TeamRepository::class, EloquentTeamRepository::class);
     }
 
     /**
